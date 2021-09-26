d6c902fab13f1e61aeaf56c5b571e985a260af88
Melanie5710 <melma2459@gmail.com>
2021-09-25 20:41:47 -0500

    Servo Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
@@ -10,12 +10,13 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name = "MecanumDrive", group = "Quackology")
 public class MecanumTeleop extends OpMode {
     private DcMotorEx frontLeft, frontRight, backLeft, backRight;
+    private Servo servo;
+
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
     static final double MAX_POS     =  1.0;     // Maximum rotational position
     static final double MIN_POS     =  0.0;     // Minimum rotational position
-    // Define class members
-    Servo servo;
+
     double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
     boolean rampUp = true;
 
@@ -31,6 +32,7 @@ public class MecanumTeleop extends OpMode {
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
         // Connect to servo (Assume PushBot Left Hand)
         // Change the text in quotes to match any servo name on your robot.
         servo = hardwareMap.get(Servo.class, "left_hand");
@@ -58,6 +60,7 @@ public class MecanumTeleop extends OpMode {
         frontRight.setPower(frontRightPower);
         backLeft.setPower(backLeftPower);
         backLeft.setPower(backRightPower);
+
         // slew the servo, according to the rampUp (direction) variable.
         if (rampUp) {
             // Keep stepping up until we hit the max value.
@@ -75,14 +78,48 @@ public class MecanumTeleop extends OpMode {
                 rampUp = !rampUp;  // Switch ramp direction
             }
         }
+
         telemetry.addData("Motors", "frontLeft (%.2f), frontRight (%.2f), backLeft (%.2f), backRight(%.2f)",
                 frontLeftPower, frontRightPower, backLeftPower, backRightPower);
         telemetry.addData("Servo Position", "%5.2f", position);
         telemetry.update();
+
         // Set the servo to the new position and pause;
+        servo.setPosition(position);
+        sleep(CYCLE_MS);
+        idle();
+    }
+
+
+    /**
+     * Puts the current thread to sleep for a bit as it has nothing better to do. This allows other
+     * threads in the system to run.
+     *
+     * <p>One can use this method when you have nothing better to do in your code as you await state
+     * managed by other threads to change. Calling idle() is entirely optional: it just helps make
+     * the system a little more responsive and a little more efficient.</p>
+     *
+     * @see #opModeIsActive()
+     */
+    public final void idle() {
+        // Otherwise, yield back our thread scheduling quantum and give other threads at
+        // our priority level a chance to run
+        Thread.yield();
+    }
+
+    /**
+     * Sleeps for the given amount of milliseconds, or until the thread is interrupted. This is
+     * simple shorthand for the operating-system-provided {@link Thread#sleep(long) sleep()} method.
+     *
+     * @param milliseconds amount of time to sleep, in milliseconds
+     * @see Thread#sleep(long)
+     */
+    public final void sleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
 }
-        //servo.setPosition(position);
-        //sleep(CYCLE-MS);
-        //idle();
