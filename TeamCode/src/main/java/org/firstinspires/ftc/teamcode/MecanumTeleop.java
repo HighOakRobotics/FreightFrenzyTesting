a5e7a0b68f21a532915c19328e68d0f478ed78f7
Melanie5710 <melma2459@gmail.com>
2021-10-17 12:45:24 -0500

    Wrist and Claw code for controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
@@ -9,8 +9,8 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "MecanumDrive", group = "Quackology")
 public class MecanumTeleop extends OpMode {
-    private DcMotorEx frontLeft, frontRight, backLeft, backRight, carouselMotor;
-    private Servo servo;
+    private DcMotorEx frontLeft, frontRight, backLeft, backRight, carousel;
+//    private Servo servo;
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
@@ -18,7 +18,11 @@ public class MecanumTeleop extends OpMode {
     static final double MIN_POS     =  0.0;     // Minimum rotational position
 
     double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double clawposition = 0.5;
+    double wristposition = 0.7;
     boolean rampUp = true;
+    Servo clawServo;
+    Servo wristServo;
 
     @Override
     public void init() {
@@ -29,14 +33,16 @@ public class MecanumTeleop extends OpMode {
         frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
         backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
         backRight = hardwareMap.get(DcMotorEx.class, "backRight");
-        carouselMotor = hardwareMap.get(DcMotorEx.class, "carouselMotor");
+        carousel = hardwareMap.get(DcMotorEx.class, "carousel");
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Connect to servo (Assume PushBot Left Hand)
         // Change the text in quotes to match any servo name on your robot.
-        servo = hardwareMap.get(Servo.class, "left_hand");
+//        servo = hardwareMap.get(Servo.class, "left_hand");
+        clawServo = hardwareMap.get(Servo.class, "clawservo");
+        wristServo = hardwareMap.get(Servo.class,"wristservo");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -48,11 +54,10 @@ public class MecanumTeleop extends OpMode {
         double drive;
         double strafe;
         double turn;
-        boolean spin;
         drive = -gamepad1.left_stick_y;
         strafe = gamepad1.left_stick_x;
         turn = gamepad1.right_stick_x;
-        spin = gamepad1.y;
+
 
 
         double frontLeftPower = Range.clip(drive + strafe + turn, -1.0, 1.0);
@@ -65,35 +70,60 @@ public class MecanumTeleop extends OpMode {
         backLeft.setPower(backLeftPower);
         backLeft.setPower(backRightPower);
 
-        while (spin == true){
-            carouselMotor.setPower(1.0);
-        }
+//        if(gamepad1.x){
+//            carousel.setPower(0.5);
+//        }
+//        if(gamepad1.a){
+//            carousel.setPower(0);
+//        }`
+//        while (carousel == true) {
+//            carouselMotor.setPower(1.0);
+//        }
 
-        // slew the servo, according to the rampUp (direction) variable.
-        if (rampUp) {
-            // Keep stepping up until we hit the max value.
-            position += INCREMENT ;
-            if (position >= MAX_POS ) {
-                position = MAX_POS;
-                rampUp = !rampUp;   // Switch ramp direction
-            }
+
+//        while (spin == true){
+//            carouselMotor.setPower(1.0);
+//        }
+//
+//        // slew the servo, according to the rampUp (direction) variable.
+//        if (rampUp) {
+//            // Keep stepping up until we hit the max value.
+//            position += INCREMENT ;
+//            if (position >= MAX_POS ) {
+//                position = MAX_POS;
+//                rampUp = !rampUp;   // Switch ramp direction
+//            }
+//        }
+//        else {
+//            // Keep stepping down until we hit the min value.
+//            position -= INCREMENT ;
+//            if (position <= MIN_POS ) {
+//                position = MIN_POS;
+//                rampUp = !rampUp;  // Switch ramp direction
+//            }
+//        }
+        if(gamepad2.a){
+            clawposition = 1.0;
         }
-        else {
-            // Keep stepping down until we hit the min value.
-            position -= INCREMENT ;
-            if (position <= MIN_POS ) {
-                position = MIN_POS;
-                rampUp = !rampUp;  // Switch ramp direction
-            }
+        if(gamepad2.y){
+            clawposition = 0.0;
+        }
+        if(gamepad2.b){
+            wristposition = 0.9;
+        }
+        if(gamepad2.x){
+            wristposition = 0.7;
         }
 
         telemetry.addData("Motors", "frontLeft (%.2f), frontRight (%.2f), backLeft (%.2f), backRight(%.2f)",
                 frontLeftPower, frontRightPower, backLeftPower, backRightPower);
-        telemetry.addData("Servo Position", "%5.2f", position);
+        telemetry.addData("Claw Position", "%5.2f", clawposition);
+        telemetry.addData("Wrist Position", "%5.2f", wristposition);
         telemetry.update();
 
         // Set the servo to the new position and pause;
-        servo.setPosition(position);
+        clawServo.setPosition(clawposition);
+        wristServo.setPosition(wristposition);
 //        sleep(CYCLE_MS);
 //        idle();
     }
@@ -107,7 +137,7 @@ public class MecanumTeleop extends OpMode {
      * managed by other threads to change. Calling idle() is entirely optional: it just helps make
      * the system a little more responsive and a little more efficient.</p>
      *
-     * @see #opModeIsActive()
+     *
      */
     public final void idle() {
         // Otherwise, yield back our thread scheduling quantum and give other threads at
