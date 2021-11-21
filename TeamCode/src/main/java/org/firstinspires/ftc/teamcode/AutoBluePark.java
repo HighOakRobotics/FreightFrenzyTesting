4936c1eed318aed648a9060cad857bd4d3069595
Melanie5710 <melma2459@gmail.com>
2021-11-21 14:19:02 -0600

    Auto park

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBluePark.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "AutoBluePark", group = "Quackology")
+//@Disabled
+
+public class AutoBluePark extends LinearOpMode {
+    private DcMotorEx frontLeft, frontRight, backLeft, backRight, carousel, intake;
+    private Servo wristServo;
+    private ShoulderM shoulderm;
+    private boolean upPressed;
+    private boolean downPressed;
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        carousel = hardwareMap.get(DcMotorEx.class, "carousel");
+        intake = hardwareMap.get(DcMotorEx.class,"intake");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        wristServo = hardwareMap.get(Servo.class,"wrist");
+
+        shoulderm = new ShoulderM(hardwareMap);
+        shoulderm.init();
+        upPressed = false;
+        downPressed = false;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        double drive;
+        double strafe;
+        double turn;
+        drive = -0.5;
+        strafe = 0;
+        turn = 0;
+
+
+        double frontLeftPower = Range.clip(drive + strafe + turn, -0.75, 0.75);
+        double backLeftPower = Range.clip(drive - strafe + turn, -0.75, 0.75);
+        double frontRightPower = Range.clip(drive - strafe - turn, -0.75,0.75);
+        double backRightPower = Range.clip(drive + strafe - turn, -0.75, 0.75);
+
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+
+        runtime.reset();
+        while ((runtime.seconds() < 4.0)) {
+            telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+        telemetry.addData("Motors", "frontLeft (%.2f), frontRight (%.2f), backLeft (%.2f), backRight(%.2f)",
+                frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+    }
+}
